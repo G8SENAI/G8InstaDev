3dd379dbe36f91e3015efe074476e0e3fe36bf9e
TolentinoGustavo <tolentinogustavo@gmail.com>
2021-01-22 14:40:41 -0300

    MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace G8InstaDev.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
