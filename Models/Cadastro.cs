7ed8392b7250add6032875be3bb55b0be46e92e0
Duda <pmariaeduarda5588@gmail.com>
2021-01-27 20:53:21 -0300

    .

--- Models/Cadastro.cs
+++ Models/Cadastro.cs
@@ -16,24 +16,70 @@ namespace G8InstaDev.Models
 
         Cadastro usuario = new Cadastro();
 
+        public Cadastro(){
+            CreateFolderAndFile(PATH);
+        }
+
+        public string Prepare(Cadastro c){
+            return $"{c.IdCadastro};{c.Email};{c.NomeCompleto};{c.NomeDoUsuario};{c.Senha};";
+        }
+
+        public int idCadastro(){
+            var cadastro = ReadAll();
+
+            if (cadastro.Count == 0)
+            {
+                return 1;
+            }
+
+            var codigo = cadastro[cadastro.Count - 1]. IdCadastro;
+
+            codigo ++;
+
+            return codigo;
+        }
+
         public void Create(Cadastro c)
         {
-            throw new System.NotImplementedException();
+            string[] linhas = {Prepare(c)};
+            File.AppendAllLines(PATH, linhas);
         }
 
         public List<Cadastro> ReadAll()
         {
-            throw new System.NotImplementedException();
+            List<Cadastro> cadastro = new List<Cadastro>();
+            string[] linhas = File.ReadAllLines(PATH);
+
+            foreach (var item in linhas)
+            {
+                string[] linha = item.Split(";");
+
+                Cadastro cad = new Cadastro();
+                cad.IdCadastro = int.Parse(linha[0]);
+                cad.Email = linha[1];
+                cad.NomeCompleto = linha[2];
+                cad.NomeDoUsuario = linha[3];
+                cad.Senha = linha[4];
+
+                cad.Add(cadastro);
+            }
+
+            return cadastro;
         }
 
         public void Update(Cadastro c)
         {
-            throw new System.NotImplementedException();
+            List<string> linhas = ReadAllLinesCSV(PATH);
+            linhas.RemoveAll(x => x.Split(";")[0] == cadastro.IdCadastro.ToString());
+            linhas.Add(Prepare(cadastro));
+            RewriteCSV(PATH, linhas);
         }
 
         public void Delete(int id)
         {
-            throw new System.NotImplementedException();
+            List<string> linhas = ReadAllLinesCSV(PATH);
+            linhas.RemoveAll(x => x.Split(";")[0] == id.ToString());
+            RewriteCSV(PATH, linhas);
         }
     }
 
